d9b35808bb21a3f0a799f778f4238d7da7ca515f
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2023-02-08 12:40:49 +0530

    schedules almost completed

--- components/Common/companyAdmin-user/Schedules.tsx
+++ components/Common/companyAdmin-user/Schedules.tsx
@@ -1,23 +1,49 @@
-import React from 'react'
+import moment from 'moment';
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import useSWR from "swr";
 import { getUserSchedules } from '../../../api/User/Get/user';
 import { currentUser } from '../../../redux/user/userAuthSlicer';
 
 
 function Schedules() {
     const { userId } = useSelector(currentUser)
+    const [data, setData] = useState([])
+    const [month, setMonth] = useState(new Date())
+    const [isLoading, setIsLoading] = useState(true)
     const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ];
 
-    const fetcher = async () => {
-        const getUserSchedule = await getUserSchedules(userId,new Date());
-        return getUserSchedule;
-    };
-    const { data, error, isLoading } = useSWR("getUserSchedule", fetcher);
-    if (error) return <div>Error....</div>
-    if (isLoading) return <div>Loading....</div>
+    async function fetcher() {
+        setIsLoading(true)
+        const getUserSchedule = await getUserSchedules(userId, month);
+        setData(getUserSchedule)
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        fetcher()
+    }, [])
+
+
+    function subtractMonths(date: Date, months: number) {
+        date.setMonth(date.getMonth() - months);
+        return date;
+    }
+    function addMonths(date: Date, months: number) {
+        date.setMonth(date.getMonth() + months);
+        return date;
+    }
+
+    async function handlePreviousSchedules() {
+        setMonth(subtractMonths(month, 1))
+        fetcher()
+    }
+
+    async function handleNextSchedules() {
+        setMonth(addMonths(month, 1))
+        fetcher()
+    }
 
     return (
         <div className="text-gray-700">
@@ -25,55 +51,52 @@ function Schedules() {
                 <div className="flex flex-col flex-grow">
                     <div className="flex items-center mt-4">
                         <div className="flex ml-6">
-                            <button>
+                            <button className="cursor-pointer" onClick={handlePreviousSchedules}>
                                 <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
                                 </svg>
                             </button>
-                            <button>
+                            <button className="cursor-pointer" onClick={handleNextSchedules}>
                                 <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
                                 </svg>
                             </button>
                         </div>
-                        <h2 className="ml-2 text-xl font-bold leading-none">September, 2020</h2>
+                        <h2 className="ml-2 text-xl font-bold leading-none">{moment(month).format('MMMM YYYY')}</h2>
                     </div>
-                    <div className="grid grid-cols-7 mt-4">
-                        <div className="pl-1 text-sm">Mon</div>
-                        <div className="pl-1 text-sm">Tue</div>
-                        <div className="pl-1 text-sm">Wed</div>
-                        <div className="pl-1 text-sm">Thu</div>
-                        <div className="pl-1 text-sm">Fri</div>
-                        <div className="pl-1 text-sm">Sat</div>
-                        <div className="pl-1 text-sm">Sun</div>
-                    </div>
-                    <div className="grid flex-grow w-full h-auto grid-cols-7 grid-rows-5 gap-2 pt-px mt-1 bg-gray-200 scrollbar-hide">
-
-                        <div></div>
+                    <div className="flex flex-row flex-wrap w-full h-auto gap-2 mt-1 bg-gray-200 pr-2 justify-center pt-4 pb-4">
                         {
-                            data.map((group: any) => {
-                                const date = new Date(group._id)
-                                return (
-                                    <div key={group._id} className="relative flex flex-col bg-white group scrollbar-hide">
-                                        <span className="mx-2 my-1 text-xs font-bold leading-none truncate">{monthNames[date.getMonth()]} {date.getDate()}</span>
-                                        <div className="flex flex-col px-1 py-1 overflow-auto">
-                                            {
-                                                group.objects.map((schedule:any)=>{
-                                                    const time = new Date(group._id +' '+ schedule.data.time )
-                                                    return (
-                                                        <button key={schedule.type} className="flex flex-col h-8 flex-shrink-0 px-1 text-xs hover:bg-gray-200 ">
-                                                            <span className="font-light leading-none">{time.toLocaleTimeString()}</span>
-                                                            <span className=" font-medium leading-none truncate">{schedule.type} Interview</span>
-                                                        </button>
-                                                    )
-                                                })
-                                            }
-                                        </div>
-                                    </div>
-                                )
-                            })
+                            isLoading ? (
+                                <div className="">
+                                    <div className="animate-spin rounded-full h-14 w-14 border-t-2 border-b-2 border-gray-900"></div>
+                                </div>
+                            ) : (
+                                data.map((group: any, index: number) => {
+                                    const date = new Date(group._id)
+                                    return (
+                                        <>
+                                            <div key={index} className="w-[240px] flex flex-col justify-center items-center border-purple-800 bg-white ml-2 p-4 rounded-md border">
+                                                <span className="mx-2 my-1 text-xs font-bold ">{monthNames[date.getMonth()]} {date.getDate()}</span>
+                                                <div className="">
+                                                    {
+                                                        group.objects.map((schedule: any) => {
+                                                            const time = new Date(group._id + ' ' + schedule.data.time)
+                                                            return (
+                                                                <div key={schedule.data.time} className="cursor-pointer text-center px-1 text-xs p-4 hover:bg-purple-100 rounded-md overflow-scroll scrollbar-hide">
+                                                                    <span className="font-light leading-none">{time.toLocaleTimeString()}</span>
+                                                                    <br />
+                                                                    <span className=" font-medium">{schedule.type} Interview</span>
+                                                                </div>
+                                                            )
+                                                        })
+                                                    }
+                                                </div>
+                                            </div>
+                                        </>
+                                    )
+                                })
+                            )
                         }
-
                     </div>
                 </div>
             </div>
